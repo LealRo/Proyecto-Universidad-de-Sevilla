c6a89429612debfd4e8fc524cd646e46f6de190b
BrandonRaygoza <raygoza.brandon@gmail.com>
2020-12-28 12:39:15 -0800

    28/12/2020

--- src/querySala.php
+++ src/querySala.php
@@ -1,7 +1,11 @@
 <?php
 function getSalas($con){
     
-    $query = "SELECT * FROM sala";
+    $query = 'SELECT s.idSala as Sala, s.idMaestro as Matricula, m.nombre as Nombre, s.idCuento as "ID Cuento", 
+    c.titulo as Titulo, s.clave as Clave, s.alumno1 as "Alumno 1", s.alumno2 as "Alumno 2", s.puntaje as Puntaje, 
+    s.duracion as Duracion, s.estado as Estado FROM sala s 
+    INNER JOIN maestro m ON s.idMaestro = m.idMaestro 
+    INNER JOIN cuento c ON s.idCuento = c.idCuento ;';
    
     $result = mysqli_query($con,$query);
     if(mysqli_num_rows($result)>0){
